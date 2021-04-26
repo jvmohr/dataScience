2084a1ba00e55aee27d9782e2ff7d62764eaff88
Joseph <josephvmohr@gmail.com>
2021-04-26 00:05:18 -0500

    general soccer files update

--- Soccer/General Data/soccer_table.py
+++ Soccer/General Data/soccer_table.py
@@ -3,16 +3,21 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
 
-# To grab the current table and not past ones
+# Grabs the current table and not past ones
 
-year = '2020' # switch this to command line argument
-league = 'mls'
+# TO CHANGE ------------------------------
+# switch these to command line arguments
+year = '2020' # if season is split over two years (ie 2020-2021), put the first year here
+folder_name = 'epl'
+league = 'ENG.1'
+old_csv = '2001_2020_tables.csv'
+# ----------------------------------------
 
 # GP W D L GF GA GD P
-cols = ['Place', 'Team', 'GP', 'W', 'D', 'L', 'GF', 'GA', 'GD', 'P']
+cols = ['Place', 'Team', 'GP', 'W', 'D', 'L', 'GF', 'GA', 'GD', 'P'] # may need to switch these for other leagues (but prob not)
 table_df = pd.DataFrame([], columns=cols)
 
-url = "https://www.espn.com/soccer/standings/_/league/USA.1/season/{}".format(year) # change this
+url = "https://www.espn.com/soccer/standings/_/league/{}/season/{}".format(league, year) 
 
 r = requests.get(url)
 r.raise_for_status()
@@ -35,9 +40,15 @@ temp_df = pd.DataFrame(final_arr, columns=cols)
 temp_df['Year'] = year
 table_df = table_df.append(temp_df, ignore_index=True)
 
-table_df.to_csv(os.path.join('data', league, 'tables', year + '_tables.csv'), index=False)
+# if folder doesn't exist, make it
+tpath = os.path.join('data', folder_name, 'tables')
+if not os.path.exists(tpath):
+    os.makedirs(tpath)
+    
+table_df.to_csv(os.path.join('data', folder_name, 'tables', year + '_table.csv'), index=False)
 
 # Add it to past tables
-all_tables_df = pd.read_csv(os.path.join('data', league, 'tables', '2003_2019_tables.csv'))
-all_tables_df = all_tables_df.append(table_df, ignore_index=True)
-all_tables_df.to_csv(os.path.join('data', league, 'tables', 'all_tables.csv'), index=False)
+if old_csv != '':
+    all_tables_df = pd.read_csv(os.path.join('data', folder_name, 'tables', old_csv))
+    all_tables_df = all_tables_df.append(table_df, ignore_index=True)
+    all_tables_df.to_csv(os.path.join('data', folder_name, 'tables', 'all_tables.csv'), index=False)
