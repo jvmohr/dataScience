ecb2d234e19d345626881707cf215a43a448ad90
Joseph <josephvmohr@gmail.com>
2021-09-20 16:34:30 -0500

    Mostly general soccer nbs update

---
+++ Soccer/General Data/league_data_dict.py
@@ -0,0 +1,44 @@
+league_data_dict = {
+    'English Premier League': {
+        'Code': 'eng.1',
+        'Folder': 'epl',
+        'First Year': 2001,
+        'First Year - Agg Stats': 2002,
+        'First Year - Table': 2001,
+    },
+    'Spanish La Liga': {
+        'Code': 'esp.1',
+        'Folder': 'laliga',
+        'First Year': 2004,
+        'First Year - Agg Stats': 2002,
+        'First Year - Table': 2000,
+    },
+    'German Bundesliga': {
+        'Code': 'ger.1',
+        'Folder': 'gbund',
+        'First Year': 2012,
+        'First Year - Agg Stats': 2002,
+        'First Year - Table': 2000,
+    },
+    'Italian Serie A': {
+        'Code': 'ita.1',
+        'Folder': 'ita',
+        'First Year': 2018,
+        'First Year - Agg Stats': 2001,
+        'First Year - Table': 2000,
+    },
+    'Dutch Eredivisie': {
+        'Code': 'ned.1',
+        'Folder': 'ere',
+        'First Year': 2018,
+        'First Year - Agg Stats': 2001,
+        'First Year - Table': 2000,
+    },
+    'French Ligue 1': {
+        'Code': 'fra.1', 
+        'Folder': 'frl',
+        'First Year': 2018,
+        'First Year - Agg Stats': 2002, # all good
+        'First Year - Table': 2002, # check 2001 later
+    },
+}
